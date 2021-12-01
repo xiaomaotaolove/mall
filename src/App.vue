29293119a0d40c3e58548023c861a670ad0ca439
210348672 <chengtdl@cn.ibm.com>
2021-12-01 15:09:53 +0800

    modify tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,20 @@
 <template>
   <div id="app">
-    <TabBar>
-      <TabBarItem path="/home" activeColor="blue">
-        <img slot="item-icon" src="~assets/img/tabbar/home.jpg" alt=".." />
-        <img slot="item-icon-active" src="~assets/img/tabbar/shopping-car.jpg" alt=".." />
-        <div slot="item-text">首页</div>
-      </TabBarItem>
-      <TabBarItem path="/category"  activeColor="blue">
-        <img slot="item-icon" src="~assets/img/tabbar/shopping-car.jpg" alt=".." />
-        <img slot="item-icon-active" src="~assets/img/tabbar/home.jpg" alt=".." />
-        <div slot="item-text">分类</div>
-      </TabBarItem>
-      <TabBarItem path="/cart">
-        <img slot="item-icon" src="~assets/img/tabbar/shopping-car.jpg" alt=".." />
-        <img slot="item-icon-active" src="~assets/img/tabbar/home.jpg" alt=".." />
-        <div slot="item-text">购物车</div>
-      </TabBarItem>
-      <TabBarItem path="/profile">
-        <img slot="item-icon" src="~assets/img/tabbar/shopping-car.jpg" alt=".." />
-        <img slot="item-icon-active" src="~assets/img/tabbar/home.jpg" alt=".." />
-        <div slot="item-text">profile</div>
-      </TabBarItem>
-    </TabBar>
+    <BottomBar></BottomBar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import TabBar from "components/tabbar/TabBar.vue";
-import TabBarItem from "components/tabbar/TabBarItem.vue";
+import BottomBar from "components/content/bottomTabBar/BottomBar.vue";
 export default {
   name: "App",
   components: {
-    TabBar,
-    TabBarItem,
+    BottomBar
   },
 };
 </script>
 
 <style>
-  @import "~assets/css/base.css";
+@import "~assets/css/base.css";
 </style>
---
+++ src/components/content/bottomTabBar/BottomBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <TabBar>
+    <TabBarItem path="/home" activeColor="blue">
+      <img slot="item-icon" src="~assets/img/tabbar/home.jpg" alt=".." />
+      <img
+        slot="item-icon-active"
+        src="~assets/img/tabbar/shopping-car.jpg"
+        alt=".."
+      />
+      <div slot="item-text">首页</div>
+    </TabBarItem>
+    <TabBarItem path="/category" activeColor="blue">
+      <img
+        slot="item-icon"
+        src="~assets/img/tabbar/shopping-car.jpg"
+        alt=".."
+      />
+      <img slot="item-icon-active" src="~assets/img/tabbar/home.jpg" alt=".." />
+      <div slot="item-text">分类</div>
+    </TabBarItem>
+    <TabBarItem path="/cart">
+      <img
+        slot="item-icon"
+        src="~assets/img/tabbar/shopping-car.jpg"
+        alt=".."
+      />
+      <img slot="item-icon-active" src="~assets/img/tabbar/home.jpg" alt=".." />
+      <div slot="item-text">购物车</div>
+    </TabBarItem>
+    <TabBarItem path="/profile">
+      <img
+        slot="item-icon"
+        src="~assets/img/tabbar/shopping-car.jpg"
+        alt=".."
+      />
+      <img slot="item-icon-active" src="~assets/img/tabbar/home.jpg" alt=".." />
+      <div slot="item-text">profile</div>
+    </TabBarItem>
+  </TabBar>
+</template>
+
+<script>
+import TabBar from "components/common/tabbar/TabBar.vue";
+import TabBarItem from "components/common/tabbar/TabBarItem.vue";
+export default {
+  name: "BottomBar",
+  components: { TabBar, TabBarItem },
+};
+</script>
+
+<style>
+@import "~assets/css/base.css";
+</style>
